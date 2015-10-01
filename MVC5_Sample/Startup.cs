078f2f18366c915166897e6b3123560dbe0fbdea
exp007srs <stalin.crr@gmail.com>
2015-10-01 21:52:23 +0530

    Added initial default scaffolded project contents.

---
+++ MVC5_Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Sample.Startup))]
+namespace MVC5_Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
