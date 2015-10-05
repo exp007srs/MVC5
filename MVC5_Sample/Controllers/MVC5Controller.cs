40258864f4fce96f74956c4ce074b33a32556e18
exp007srs <stalin.crr@gmail.com>
2015-10-05 20:27:39 +0530

    Added Controller - MVC5

---
+++ MVC5_Sample/Controllers/MVC5Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_Sample.Controllers
+{
+    public class MVC5Controller : Controller
+    {
+        // GET: MVC5
+        //public string Index()
+        //{
+        //    return "<b>DEFAULT ACTION</b>";
+        //    //return View();
+        //}
+
+        // GET: MVC5
+
+        public ActionResult Index()
+        {
+           return View(); //Returns Razor view named Index.cshtml.
+        }
+        
+        // GET: /MVC5/Welcome/
+
+        // "name" and "numTimes" -> Query string paramters.
+        // "ID" -> URL paramter.
+        public string Welcome(string name, int numTimes=1, int ID=1)
+        {
+            //return "<b>WELCOME ACTION</b>";
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is:" + numTimes + " and ID: " + ID);
+        }
+    }
+}
