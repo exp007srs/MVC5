c296de2d892a3eb05a2b846ac30fd43d3207d6f7
exp007srs <stalin.crr@gmail.com>
2015-10-07 21:57:15 +0530

    Added Movies Controller -> Edit Screen

---
+++ MVC5_Sample/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;  //Entity Framework Namespace - DbContext | DbSet
+
+namespace MVC5_Sample.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // Entity Framework - Database Context
+    // FETCH, INSERT and UPDATE
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
